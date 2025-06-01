911ac7daf58527ab3b8b275c4dc6da1a5d5ea95e
Forman37 <forman3@Me.com>
2025-06-01 15:41:08 -0400

    Finished first of 5 blocks

--- project1.pl
+++ project1.pl
@@ -1,10 +1,18 @@
 asu(LeftRightMargin, BottomTopMargin, SpaceBetweenCharacters, FontSize) :-
 	HorizontalSize is (LeftRightMargin * 2) + (SpaceBetweenCharacters * 2) + (FontSize * 3 * 3),
     write('Horizontal Size : '), write(HorizontalSize), nl,
-    bounding_box_top(0, HorizontalSize + 2),
-    initial_newLines(0, BottomTopMargin).
+    bounding_box_top(0, HorizontalSize + 2), nl,
+    initial_newLines(0, BottomTopMargin, HorizontalSize),
+
+    /*
+    There are 5 blocks for the height. So we will make 5
+    block predicates that does each function inside of each letter.
+    Our Spaces for the BottomTopMargin will be done seperately
+    */
+
+    first_block(0, LeftRightMargin, SpaceBetweenCharacters, FontSize).
     
-    
+
 
 bounding_box_top(Current, Max) :-
     Current < Max,
@@ -15,13 +23,60 @@ bounding_box_top(Current, Max) :-
 bounding_box_top(Current, Max) :-
     Current >= Max.
 
-initial_newLines(Current, Max) :-
+
+
+
+initial_newLines(Current, Max, Spaces) :-
     Current < Max,
+    write('|'),
+    print_space(0, Spaces),
+    write('|'),
     nl,
     Next is Current + 1,
-    initial_newLines(Next, Max).
+    initial_newLines(Next, Max, Spaces).
 
-initial_newLines(Current, Max) :-
+initial_newLines(Current, Max, Spaces) :-
     Current >= Max.
         
     
+first_block(Iteration, LeftRightMargin, SpaceBetweenCharacters, FontSize) :-
+    Iteration < FontSize,
+    write('|'),
+    print_space(0, LeftRightMargin),
+    print_star(0, FontSize * 3),
+    print_space(0, SpaceBetweenCharacters),
+    print_star(0, FontSize * 3),
+    print_space(0, SpaceBetweenCharacters),
+    print_star(0, FontSize),
+    print_space(0, FontSize),
+    print_star(0, FontSize),
+    print_space(0, LeftRightMargin),
+    write('|'),
+    nl,
+    Next is Iteration + 1,
+    first_block(Next, LeftRightMargin, SpaceBetweenCharacters, FontSize).
+
+first_block(Iteration, LeftRightMargin, SpaceBetweenCharacters, FontSize) :-
+    Iteration >= FontSize.
+
+
+
+print_star(Current, Max) :-
+    Current < Max,
+    write('*'),
+    Next is Current + 1,
+    print_star(Next, Max).
+    
+print_star(Current, Max) :-
+    Current >= Max.
+
+
+
+print_space(Current, Max) :-
+    Current < Max,
+    write(' '),
+    Next is Current + 1,
+    print_space(Next, Max).
+    
+print_space(Current, Max) :-
+    Current >= Max.
